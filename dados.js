706a568e3c9dc9cccad9ddb509b0a50226fb4b30
Ivandeltoide <ivandelguercio10@gmail.com>
2024-09-10 15:50:55 -0300

    incluindo novos veículos

--- dados.js
+++ dados.js
@@ -1,17 +1,68 @@
-let dados=[
-    {titulo :"Impala",
-        subtitulo:"O Chevrolet Impala: Um Clássico Americano",
-        descricao :"O Chevrolet Impala é um ícone da indústria automobilística americana, conhecido por seu design elegante e desempenho confiável. Introduzido em 1958, o Impala rapidamente se tornou um dos carros mais vendidos nos Estados Unidos, conquistando o coração de milhões de motoristas ao redor do mundo. Com diversas gerações e modelos, o Impala se destacou por sua combinação de conforto, potência e estilo, tornando-se um símbolo de status e tradição na cultura automotiva.",
-        link :"https://pt.wikipedia.org/wiki/Chevrolet_Impala"
+let dados = [
+    {
+        titulo: "Impala",
+        subtitulo: "O Chevrolet Impala: Um Clássico Americano",
+        descricao: "O Chevrolet Impala é um ícone da indústria automobilística americana, conhecido por seu design elegante e desempenho confiável. Introduzido em 1958, o Impala rapidamente se tornou um dos carros mais vendidos nos Estados Unidos, conquistando o coração de milhões de motoristas ao redor do mundo. Com diversas gerações e modelos, o Impala se destacou por sua combinação de conforto, potência e estilo, tornando-se um símbolo de status e tradição na cultura automotiva.",
+        link: "https://pt.wikipedia.org/wiki/Chevrolet_Impala"
     },
-    {titulo :"Corvette",
-        subtitulo :"O Chevrolet Corvette: Um Esporte Americano",
-        descricao :"O Chevrolet Corvette é um ícone dos carros esportivos americanos, conhecido por seu desempenho e design agressivo. Desde sua introdução em 1953, o Corvette tem sido sinônimo de potência e emoção ao volante.",
-        link :"https://pt.wikipedia.org/wiki/Chevrolet_Corvette"
+    {
+        titulo: "Corvette",
+        subtitulo: "O Chevrolet Corvette: Um Esporte Americano",
+        descricao: "O Chevrolet Corvette é um ícone dos carros esportivos americanos, conhecido por seu desempenho e design agressivo. Desde sua introdução em 1953, o Corvette tem sido sinônimo de potência e emoção ao volante.",
+        link: "https://pt.wikipedia.org/wiki/Chevrolet_Corvette"
     },
-    {titulo :"Volkswagen Beetle",
-        subtitulo :"O Volkswagen Beetle: Um Clássico Atemporal",
-        descricao :"O Volkswagen Beetle, popularmente conhecido como Fusca, é um ícone do design automotivo e um símbolo da cultura pop. Produzido por décadas, o Beetle conquistou o coração de milhões de pessoas ao redor do mundo com seu design simpático e confiabilidade.",
-        link :"https://pt.wikipedia.org/wiki/Volkswagen_Beetle"
+    {
+        titulo: "Volkswagen Beetle",
+        subtitulo: "O Volkswagen Beetle: Um Clássico Atemporal",
+        descricao: "O Volkswagen Beetle, popularmente conhecido como Fusca, é um ícone do design automotivo e um símbolo da cultura pop. Produzido por décadas, o Beetle conquistou o coração de milhões de pessoas ao redor do mundo com seu design simpático e confiabilidade.",
+        link: "https://pt.wikipedia.org/wiki/Volkswagen_Beetle"
+    },
+    {
+        titulo: "Mustang",
+        subtitulo: "O Ford Mustang: Um Ícone Americano",
+        descricao: " Ford Mustang, um dos carros esportivos mais icônicos da história, representa a liberdade e a paixão por automóveis. Lançado em 1964, o Mustang rapidamente se tornou um símbolo da cultura pop americana, conquistando corações com seu design elegante e motor potente. Ao longo das décadas, o Mustang passou por diversas transformações, mas sempre manteve sua essência esportiva e a capacidade de emocionar os entusiastas de carros. Seja nas pistas de corrida ou nas ruas, o Mustang continua sendo um ícone atemporal, celebrando a paixão pela velocidade e pelo estilo de vida americano.",
+        link: "https://pt.wikipedia.org/wiki/Ford_Mustang"
+    },
+    {
+        titulo: "BMW M3",
+        subtitulo: "O BMW M3: Desempenho Alemão",
+        descricao: "O BMW M3 é mais do que apenas um carro, ele representa a essência do desempenho esportivo alemão. Desde seu lançamento, o M3 se tornou um ícone entre os entusiastas de automóveis, combinando um design elegante e esportivo com uma mecânica precisa e potente. Com cada nova geração, o M3 eleva os padrões de desempenho e tecnologia, oferecendo uma experiência de condução emocionante e envolvente. Seus motores de seis cilindros em linha, com potência e torque impressionantes, proporcionam uma aceleração vigorosa e uma sonoridade inconfundível. Além disso, o M3 conta com um chassi extremamente preciso, capaz de transmitir ao motorista a sensação de estar conectado diretamente à estrada.",
+        link: "https://pt.wikipedia.org/wiki/BMW_M3"
+    },
+    {
+        titulo: "Cadillac Eldorado",
+        subtitulo: "O Cadillac Eldorado: Luxo e Elegancia",
+        descricao: "Produzido por décadas, o Eldorado se destacou por seu design imponente, motor potente e acabamento impecável. Seus modelos conversíveis, em especial, se tornaram verdadeiras obras de arte sobre rodas, com linhas fluidas e detalhes cromados que o consagraram como um símbolo de status e poder. O Eldorado marcou presença em filmes e séries de televisão, sendo escolhido por personalidades famosas e admirado por entusiastas de carros clássicos ao redor do mundo. Atualmente, modelos antigos do Eldorado são altamente cobiçados por colecionadores, preservando a história e a tradição da marca Cadillac.",
+        link: "https://pt.wikipedia.org/wiki/Cadillac_Eldorado"
+    },
+    {
+        titulo: "Chevrolet Bel Air",
+        subtitulo: "O Chevrolet Bel Air: Um Emblemático Americano",
+        descricao: "Conhecido por seu design elegante e imponente, o Bel Air se tornou um símbolo de status e prosperidade para muitos americanos. Suas linhas curvas e detalhes cromados o tornaram um carro inconfundível, e sua presença em filmes e séries de televisão contribuiu para consolidar sua fama. O Bel Air representou uma época de grandes transformações sociais e econômicas nos Estados Unidos, e até hoje é considerado um clássico atemporal, muito cobiçado por colecionadores e entusiastas de carros antigos.",
+        link: "https://pt.wikipedia.org/wiki/Chevrolet_Bel_Air"
+    },
+    {
+        titulo: "Ford Maverick",
+        subtitulo: "O Ford Maverick: Um Muscle Car Acessível",
+        descricao: "O Ford Maverick é um modelo que marcou a história da indústria automobilística, especialmente nos Estados Unidos. Conhecido por sua versatilidade e design robusto, o Maverick conquistou o coração de muitos motoristas. Nascido como um carro compacto e acessível, o Maverick se tornou um símbolo de liberdade e aventura, sendo utilizado tanto para o dia a dia quanto para atividades ao ar livre. Suas diferentes gerações e modelos, cada uma com suas características únicas, contribuíram para consolidar seu status de ícone automotivo. Com o lançamento da nova geração em 2021, a Ford resgatou o nome Maverick para apresentar uma picape compacta e moderna, que promete revolucionar o segmento.",
+        link: "https://pt.wikipedia.org/wiki/Ford_Maverick"
+    },
+    {
+        titulo: "Chevrolet Opala",
+        subtitulo: "O Chevrolet Opala: Um Ícone Nacional",
+        descricao: "O Chevrolet Opala, produzido no Brasil entre 1968 e 1992, é um dos carros mais icônicos da indústria automobilística nacional. Com design elegante e mecânica robusta, o Opala conquistou o coração dos brasileiros e se tornou sinônimo de status e confiabilidade. Suas diversas versões e motorizações, desde os modelos mais básicos até os esportivos, atenderam a diferentes públicos e marcaram gerações. O Opala é lembrado com carinho por sua presença em filmes, novelas e no dia a dia dos brasileiros, consolidando seu lugar na história do automobilismo brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Chevrolet_Opala"
+    },
+    {
+        titulo: "Ford Escort",
+        subtitulo: "O Ford Escort: Um Hatch Compacto e Esportivo",
+        descricao: "O Ford Escort foi um dos carros mais populares e marcantes da indústria automobilística brasileira. Produzido pela Ford do Brasil a partir de 1983, o Escort substituiu o Corcel e se tornou rapidamente um dos favoritos do público. Com design moderno, mecânica robusta e diversas opções de motorização, o Escort conquistou um amplo público, desde os jovens esportistas até as famílias. Suas versões esportivas, como o XR3, eram especialmente desejadas pelos amantes da velocidade. O Escort marcou época no Brasil e deixou um legado importante na história da indústria automobilística nacional.",
+        link: "https://pt.wikipedia.org/wiki/Ford_Escort"
+    },
+    {
+        titulo: "Volkswagen Passat",
+        subtitulo: "O Volkswagen Passat: Elegancia e Conforto",
+        descricao: " Produzido pela Volkswagen desde 1973, o Passat conquistou o público com seu design elegante, conforto e confiabilidade. Ao longo das gerações, o modelo se adaptou às tendências do mercado, combinando tecnologia avançada com a tradição da marca alemã. O Passat se destaca por sua dirigibilidade precisa, espaço interno generoso e acabamento de alta qualidade, tornando-o uma opção popular para aqueles que buscam um carro versátil e sofisticado. Sua posição no mercado, entre o Golf/Jetta e o Phaeton, o coloca como um dos modelos mais importantes da Volkswagen, com uma longa história de sucesso e inovação.",
+        link: "https://pt.wikipedia.org/wiki/Volkswagen_Passat"
     }
 ];
